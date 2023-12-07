6ee718cb7922161cc2fc55bf3953944d6c2a6e8a
“JACK <jack.cao@kcl.ac.uk>
2023-12-07 14:12:24 +0000

    Done

--- things/forms.py
+++ things/forms.py
@@ -1,10 +1,12 @@
 """Forms of the project."""
 from django import forms
-
+from .models import Thing
+from django.core.validators import MaxLengthValidator
 
 class ThingForm(forms.ModelForm):
-    name = forms.CharField()
-    description = forms.Textarea()
+    name = forms.CharField(max_length=35)
+    description = forms.CharField(widget=forms.Textarea, max_length=120)
     quantity = forms.NumberInput()
     class Meta:
+        model = Thing
         fields = ['name', 'description', 'quantity']
--- things/views.py
+++ things/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ThingForm
-
+from .models import Thing
 def home(request):
     if request.method == 'POST':
         form = ThingForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data.get('name')
+            description = form.cleaned_data.get('description')
+            quantity = form.cleaned_data.get('quantity')
+            thing = Thing.objects.create(name=name, description=description, quantity=quantity)
+            thing.save()
+            return redirect('home')
+    form = ThingForm()
     return render(request, 'home.html', {'form': form})
